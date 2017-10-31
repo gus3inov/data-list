e52b174dadceb24614ff152fd9338b90cc506f91
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-31 23:07:18 +0300

    return table

---
+++ src/components/Table.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+function Table (){  
+        return (
+            <table className="table table-hover table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+);
+    
+}
+
+export default Table;
