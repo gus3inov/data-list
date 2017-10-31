312a91081f7d11df6cd4ac272794136b82e66ceb
Muslim Guseinov <gusejnov1999@list.ru>
2017-10-31 23:46:00 +0300

    container for table

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="main-table">
+        <div className="main-table container">
         <Table />
         </div>
       </div>
