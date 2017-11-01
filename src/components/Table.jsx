2f4ce4594a2fc37eea0d683f8bb7335e7369c8be
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-01 20:53:13 +0300

    Render users

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import Rows from './Rows';
+// import Rows from './Rows';
 
-
-function Table () {  
+function Table ({ data }) {  
             return (
             <table className="table table-hover table-dark">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First Name</th>
-      <th scope="col">Last Name</th>
-      <th scope="col">Username</th>
+      <th scope="col" className="thBlack">#</th>
+      <th scope="col" className="thBlack">First Name</th>
+      <th scope="col" className="thBlack">Last Name</th>
+      <th scope="col" className="thBlack">email</th>
+      <th scope="col" className="thBlack">phone</th>
     </tr>
   </thead>
   <tbody>
-   <Rows />
+     {
+         data
+         .map( (val, index) => {
+        return <tr key={index}>
+      <th scope="row" className="text-align-center">{val.id}</th>
+      <td className="text-align-center">{val.firstName}</td>
+      <td className="text-align-center">{val.lastName}</td>
+      <td className="text-align-center">{val.email}</td>
+      <td className="text-align-center">{val.phone}</td>
+    </tr>
+     })
+     }
   </tbody>
 </table>
 );
