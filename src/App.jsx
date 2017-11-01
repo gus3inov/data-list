725d677f36d20718f83cd092898f85c45c0e8392
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-01 20:52:37 +0300

    json connect, state are array

--- src/App.jsx
+++ src/App.jsx
@@ -9,30 +9,18 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data: null
+      data: []
     }
   }
 
   componentDidMount(){
-    this.setState({
-      data: function(){
-        fetch('./users.json')
-  .then(function(response) {
-    alert(response.headers.get('Content-Type')); // application/json; charset=utf-8
-    alert(response.status); // 200
-
-    return response.json();
-   })
-  .then(function(user) {
-    alert(user.id); // iliakan
-  })
-  .catch( alert );
-      }
-    });
-  }
-
-  con(){
-
+        fetch('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
+        .then(res =>  res.json())
+        .then(data =>{
+          this.setState({
+            data: data  
+          })
+        })
   }
 
   render() {
@@ -43,7 +31,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <div className="main-table container">
-        <Table />
+        <Table data={this.state.data}/>
         </div>
       </div>
     );
