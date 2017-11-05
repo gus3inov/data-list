c1fc61c93d345ff46aefd5db8b1144e8a6a97543
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-05 12:43:35 +0300

    Render table and main content to Main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Table from './components/Table';
+import Header from './components/Header';
+import Main from './components/Main';
 
 
 class App extends Component {
@@ -9,52 +9,26 @@ class App extends Component {
     super(props);
 
     this.state = {
-      data: [],
-      description: null,
-      address: null,
-      city: null,
-      provinces: null,
-      indexCity: null
+      data: []
     }
-
-    this.handleClickUser = this.handleClickUser.bind(this);
   }
 
-  componentDidMount(){
+componentDidMount(){
         fetch('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
         .then(res =>  res.json())
         .then(data =>{
           this.setState({
-            data: data  
+            data  
           })
+
+          console.log(this.state.data)
         })
   }
-
-  handleClickUser(){
-    this.setState({
-      description: this.state.dat a;
-    });
-  } 
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <div className="main-table container">
-        <div className="row">
-        <div className="col-md-6">
-        <Table data={this.state.data} onClick={handleClickUser}/>
-        </div>
-        <div className="col-md-6">
-        <div className="results">
-        
-        </div>
-        </div>
-        </div>
-        </div>
+        <Header />
+        <Main data = { this.state.data }/>
       </div>
     );
   }
