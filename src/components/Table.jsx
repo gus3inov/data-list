df63a9e121bb616caee7939d4f306a4dcf1a417b
Muslim Guseinov <gusejnov1999@list.ru>
2017-11-06 12:36:29 +0300

    Sort id

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,34 +1,73 @@
-import React from 'react';
-// import Rows from './Rows';
+import React, { Component } from 'react';
+import Rows from './Rows';
 
-function Table ({ data }) {  
-            return (
+class Table extends Component { 
+  constructor(props){
+    super(props);
+
+      this.state = {
+        sort: false
+      }
+  }
+
+  handleSort = () => {
+    this.setState({
+      sort: !this.state.sort
+    });
+  }
+
+            render(){
+              return (
             <table className="table table-hover table-dark">
   <thead>
     <tr>
-      <th scope="col" className="thBlack">#</th>
+      <th scope="col" style={{ cursor: 'pointer' }} className="thBlack" onClick = { this.handleSort }>
+      #  <span className={ this.state.sort ?  `glyphicon glyphicon-hand-up` : `glyphicon glyphicon-hand-down`}></span>
+      </th>
       <th scope="col" className="thBlack">First Name</th>
       <th scope="col" className="thBlack">Last Name</th>
       <th scope="col" className="thBlack">email</th>
       <th scope="col" className="thBlack">phone</th>
     </tr>
   </thead>
-  <tbody>
+  <tbody> 
      {
-         data
-         .map( (val, index) => {
-        return <tr key={index}>
-      <th scope="row" className="text-align-center">{val.id}</th>
-      <td className="text-align-center">{val.firstName}</td>
-      <td className="text-align-center">{val.lastName}</td>
-      <td className="text-align-center">{val.email}</td>
-      <td className="text-align-center">{val.phone}</td>
-    </tr>
-     })
-     }
-  </tbody>
+       this.props.data.sort((a, b) => {
+  if(this.state.sort){
+    if (a.id > b.id) {
+    return 1;
+  }
+  if (a.id < b.id) {
+    return -1;
+  }
+  }else{
+if (a.id < b.id) {
+    return 1;
+  }
+  if (a.id > b.id) {
+    return -1;
+  }
+  }
+  // a должно быть равным b
+  return 0;
+}).map((value, index) =>{
+        return <Rows 
+          id        = { value.id } 
+          key     =   {index}
+          firstName = { value.firstName } 
+          lastName  = { value.lastName }
+          email     = { value.email }
+          phone     = { value.phone }
+          adress    = { value.adress }
+          description     = { value.description }
+         />
+       })
+
+      }
+       </tbody>
 </table>
 );
+            }
     
 }
 
